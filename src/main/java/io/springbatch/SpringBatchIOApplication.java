0967b5108c4070a58e87f61a71b4e0867abd5585
willharmer <willjharmer@gmail.com>
2016-10-09 15:11:12 +0100

    Renamed project and cleaned up where EnableBatchProcessing annotation resides.

--- src/main/java/io/springbatch/SpringBatchIOApplication.java
+++ src/main/java/io/springbatch/SpringBatchIOApplication.java
@@ -1,12 +1,14 @@
 package io.springbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo2Application {
+@EnableBatchProcessing
+public class SpringBatchIOApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo2Application.class, args);
+		SpringApplication.run(SpringBatchIOApplication.class, args);
 	}
 }
--- src/main/java/io/springbatch/configuration/JobConfiguration.java
+++ src/main/java/io/springbatch/configuration/JobConfiguration.java
@@ -1,14 +1,9 @@
 package io.springbatch.configuration;
 
-import com.sun.tracing.ProbeName;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +14,6 @@ import org.springframework.context.annotation.Configuration;
  */
 
 @Configuration
-@EnableBatchProcessing
 public class JobConfiguration {
 
     @Autowired
